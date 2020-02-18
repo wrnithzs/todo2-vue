1ffc095d2c1cb1ce16573c8c92c1acac7bb44df3
wrnithzs <baby_mind2@hotmail.com>
2020-02-18 17:15:35 +0700

    :crown: delete and show/hide create form

--- todo.vue
+++ todo.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app" class="container">
-    <nav class="navbar navbar-expand-md navbar-light bg-light mb-4">
-      <a class="navbar-brand" href="#">Navbar</a>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link " href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " v-on:click="loadTodo(), showCreate =false, showTodo =true">Todo List</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" v-on:click="showCreate = true , showTodo = false">Create </a>
+      </li>
+    </ul>
+    <!-- <nav class="navbar navbar-expand-md navbar-light bg-light mb-4">
+      <a class="nav-link active" href="#">Home</a>
       <button
         type="button"
         data-toggle="collapse"
@@ -22,49 +33,66 @@
               <span class="sr-only">(current)</span>
             </a>
           </li>
-            <li class="nav-item ">
+          <li class="nav-item">
             <a v-on:click="showCreate = true , showTodo = false" class="nav-link" href="#">
-              Create Todo
+              Create
               <span class="sr-only">(current)</span>
             </a>
           </li>
         </ul>
       </div>
-    </nav>
-    <br/>
-      <div v-if="showTodo">
+    </nav>-->
+    <br />
+    <div v-if="showTodo">
       <div v-for="(todo, index) in todos" v-bind:key="todo.id" class="card mb-1">
         <div class="card-body">
           <h5 class="card-title">Todo {{index+1}}</h5>
-          <p class="card-text">{{todo.task }}</p>
-          <p class="card-text">{{todo.timestamp }}</p>
+          <p class="card-text">title: {{todo.task }}</p>
+          <p class="card-text">details:{{todo.details}}</p>
+          <!-- <p class="card-text">{{todo.timestamp }}</p> -->
           <div class="row">
             <div class="col-auto-mr-auto">
               <button type="button" class="btn btn-info">Edit</button>
               &nbsp;
-              <button @click="removeTodo(todo.id)" type="button" class="btn btn-danger">Delete</button>
+              <button
+                v-on:click="removeTodo(todo.id)"
+                type="button"
+                class="btn btn-danger"
+              >Delete</button>
             </div>
-            <div class="col-auto">&nbsp;
-              <button class="btn btn-outline-info">Up</button>
+            <div class="col-auto">
+              &nbsp;
+              <button
+                v-if=" index !== 0"
+                class="btn btn-outline-info"
+                v-on:click="moveUp(index)"
+              >Up</button>&nbsp;
+              <button
+                v-if=" index !== 0"
+                class="btn btn-outline-info"
+                v-on:click="moveDown(index)"
+              >Down</button>
             </div>
           </div>
-        
         </div>
       </div>
-      </div>
-      <div v-if="showCreate">
+    </div>
+    <div v-if="showCreate">
       <div class="form-group">
-        <label for="exampleInputEmail1">Input Todo</label>
-        <input type="text"  class="form-control"/>
-        <label >Descriptions</label>
-        <input type="text"  class="form-control"/>
+        <label>Input Todo</label>
+        <input type="text" v-model="newTask" class="form-control" />
+        <br />
+        <label>Details</label>
+        <input type="text" v-model="newDetail" class="form-control" />
       </div>
-      <button type="button" class="btn btn-warning" v-on:click="loadTodo(),showCreate =false, showTodo =true">Cancel</button>&nbsp;
+      <button
+        type="button"
+        class="btn btn-warning"
+        v-on:click="loadTodo(),showCreate =false, showTodo =true"
+      >Cancel</button>&nbsp;
       <button type="button" class="btn btn-primary" v-on:click="addTodo()">Save</button>
-      </div>
-    <br/>
-
-
+    </div>
+    <br />
   </div>
 </template>
 
@@ -76,15 +104,15 @@
 import { firebase } from "@firebase/app";
 import "@firebase/firestore";
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyDgf7t1b116sMNtXgJ0lnlIssVlYZAZvu4",
-    authDomain: "vue-todo-1c732.firebaseapp.com",
-    databaseURL: "https://vue-todo-1c732.firebaseio.com",
-    projectId: "vue-todo-1c732",
-    storageBucket: "vue-todo-1c732.appspot.com",
-    messagingSenderId: "599727970654",
-    appId: "1:599727970654:web:cd2027969f09f4dd6dd7cf"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyDgf7t1b116sMNtXgJ0lnlIssVlYZAZvu4",
+  authDomain: "vue-todo-1c732.firebaseapp.com",
+  databaseURL: "https://vue-todo-1c732.firebaseio.com",
+  projectId: "vue-todo-1c732",
+  storageBucket: "vue-todo-1c732.appspot.com",
+  messagingSenderId: "599727970654",
+  appId: "1:599727970654:web:cd2027969f09f4dd6dd7cf"
+};
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -93,32 +121,38 @@ export default {
   data() {
     return {
       todos: [],
-      newTask: " ",
+      newTask: "",
+      newDetail: "",
+      editTask: " ",
+      editDetails: " ",
       showCreate: false,
-      showTodo: true,
-    }
+      showTodo: true
+    };
   },
   firestore() {
     return {
       todos: db.collection("todos")
-    }
+    };
   },
   methods: {
     addTodo() {
       db.collection("todos").add({
         task: this.newTask,
-        timestamp: new Date()
+        details: this.newDetail
       });
       this.loadTodo();
     },
     loadTodo() {
       let todolist = [];
-      db.collection("todos").get().then(function(querySnapshot) {
+      db.collection("todos")
+        .get()
+        .then(function(querySnapshot) {
           querySnapshot.forEach(function(doc) {
             let todo = {
               id: doc.id,
               task: doc.data().task,
-              timestamp: doc.data().timestamp.toDate()
+              details: doc.data().details
+              // timestamp: doc.data().timestamp.toDate()
             };
             todolist.push(todo);
           });
@@ -135,11 +169,31 @@ export default {
         });
       this.loadTodo();
     },
+    // editTodo() {
+    //   db.collection("todos").doc(collectionsID).set({
+    //   task: //Valueee,
+    //   details: //Valueeee,
+    // }
+    moveUp(index) {
+      if (index == 0) {
+        return;
+      }
+      let todo = this.todos[index];
+      this.todos.splice(index, 1);
+       this.todos.splice(index -1, 0, todo);
+    },
+    moveDown(index) {
+      if (index == this.todos.length - 1) {
+        return;
+      }
+      let todo = this.todos[index];
+      this.todos.splice(index, 1);
+      this.todos.splice(index + 1, 0, todo);
+    }
   },
   mounted() {
     this.loadTodo();
-  },
-
+  }
 };
 </script>
 
